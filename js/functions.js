32675a5d53415afd15b596a06f6515901c114b49
cdDiaCo <claudia_cocioaba@yahoo.ro>
2016-09-13 19:06:24 +0300

    add jquery

--- js/functions.js
+++ js/functions.js
@@ -2,16 +2,17 @@
  * Created by claudia on 12.09.2016.
  */
 
-function rotateTriangleIcon(elem) {
-    var currentClass = elem.childNodes[0].className;
-    var res = new RegExp("left").test(currentClass);
+$(document).ready(function(){
+    $(".triangleIconWrapper").click(rotateTriangleIcon);
+});
 
-    if( res ) {
-        // remove 'left' class
-        elem.childNodes[0].className = currentClass.replace(new RegExp('\\b' + 'left' + '\\b'), '');
+function rotateTriangleIcon() {
+    var triangleIcon =  $(this).find("i");
+
+    if( triangleIcon.hasClass("left") ) {
+        triangleIcon.removeClass("left");
     }
     else {
-        // add 'left' class
-        elem.childNodes[0].className += ' left';
+        triangleIcon.addClass("left");
     }
 }
