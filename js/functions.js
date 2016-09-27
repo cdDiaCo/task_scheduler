276c01cd01d6ce80c5560248ea496cb481a88b46
cdDiaCo <claudia_cocioaba@yahoo.ro>
2016-09-27 22:43:31 +0300

    refactoring

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,139 @@
-/**
- * Created by claudia on 12.09.2016.
- */
-
 $(document).ready(function(){
     $(".triangleIconWrapper").click(rotateTriangleIcon);
     $("#search_box_wrapper").find("input").keyup(searchForExistingTasks_ajax);
 });
 
-var THESITENAME = (function() {
+var THESITENAME = {};
+
+THESITENAME.CURRENT_SECTION = (function() {
+    var currentSection;
+
+    return {
+        set: function(newVal) {
+            currentSection = newVal;
+        },
+        get: function() {
+            return currentSection;
+        }
+    }
+}());
+
+THESITENAME.MATCHING_TASKS = (function() {
+    var matchedTasks;
+
+    return {
+        set: function(newVal) {
+            matchedTasks = newVal;
+        },
+        get: function() {
+            return matchedTasks;
+        },
+        display: function() {
+            THESITENAME.DISPLAYED_TASKS.removeUnnecessary(matchedTasks);
+
+            if( (!matchedTasks || matchedTasks.length === 0)  && !THESITENAME.NO_MATCHING_TASKS.isDisplayed() ) {
+                // there are no matched tasks
+                THESITENAME.NO_MATCHING_TASKS.display();
+            }
+            else {
+                //there are matched tasks
+                for (var i=0; i<matchedTasks.length; i++) {
+                    if(! THESITENAME.DISPLAYED_TASKS.isDisplayed(matchedTasks[i]) ) {
+                        THESITENAME.MATCHING_TASKS.add(matchedTasks[i]);
+                    }
+                }
+            }
+        },
+        add: function(task) {
+            var section = THESITENAME.CURRENT_SECTION.get();
+            section.find(".matchingTasks").append($('<input>')
+                .prop('type', 'button')
+                .val("" + task)
+                .addClass("taskButton")
+            );
+        }
+    }
+}());
+
+THESITENAME.NO_MATCHING_TASKS = (function() {
+    // this is about the "no results were found" text
     var noMatchingTasksText = false;
 
     return {
-        isDisplayedNoMatchingTasksText: function() {
+        isDisplayed: function() {
             return noMatchingTasksText;
         },
-        set_isDisplayedNoMatchingTasksText: function(newVal) {
+        set: function(newVal) {
             noMatchingTasksText = newVal;
+        },
+        display: function() {
+            var section = THESITENAME.CURRENT_SECTION.get();
+            $(section).find(".content_wrapper").append($('<p>')
+                .text("no results were found")
+                .addClass("noMatchingTasks"));
+
+            THESITENAME.NO_MATCHING_TASKS.set(true);
+        },
+        remove: function() {
+            var section = THESITENAME.CURRENT_SECTION.get();
+            $(section).find(".noMatchingTasks").remove();
+            THESITENAME.NO_MATCHING_TASKS.set(false);
         }
     };
-
 }());
+
+THESITENAME.DISPLAYED_TASKS = (function() {
+
+    return {
+        get: function() {
+            var section = THESITENAME.CURRENT_SECTION.get();
+            return $(section).find(".taskButton");
+        },
+        isDisplayed: function(task) {
+            var displayedTaskButtons = THESITENAME.DISPLAYED_TASKS.get();
+            for (var i=0; i<displayedTaskButtons.length; i++) {
+                if( $(displayedTaskButtons[i]).val() === task ) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        removeUnnecessary: function(matchedTasks) {
+            var displayedTaskButtons = THESITENAME.DISPLAYED_TASKS.get();
+
+            if( matchedTasks.length === 0 ) {
+                //there are no matched tasks, so remove the ones remained from the previous search term
+                $(displayedTaskButtons).remove();
+            }
+            else {
+                if( THESITENAME.NO_MATCHING_TASKS.isDisplayed() ) {
+                    // this text is present from the previous search but now there are matching tasks
+                    // and the text has to be removed
+                    THESITENAME.NO_MATCHING_TASKS.remove();
+                }
+                if ( displayedTaskButtons.length > matchedTasks.length ) {
+                    // remove those displayed buttons that are unnecessary
+                    for (var i=0; i<displayedTaskButtons.length; i++) {
+                        var unnecessaryTask = false;
+                        for (var j=0; j<matchedTasks.length; j++) {
+                            if( $(displayedTaskButtons[i]).val() === matchedTasks[j] ) {
+                                unnecessaryTask = false;
+                                break;
+                            }
+                            else {
+                                unnecessaryTask = true;
+                            }
+                        }
+                        if (unnecessaryTask) {
+                            $(displayedTaskButtons[i]).remove();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}());
+
 
 
 function rotateTriangleIcon() {
--- js/stub_functions.js
+++ js/stub_functions.js
@@ -1,126 +1,31 @@
 
 function searchForExistingTasks_ajax() {
     var section = $("#search_tasks");
+    THESITENAME.CURRENT_SECTION.set(section);
     var searchTerm = $(this).val();
-    var matchedTasks;
-    var displayedTaskButtons = section.find(".taskButton");
 
     if( searchTerm !== "" && searchTerm !== " " ) {
         //the search ter IS NOT an empty string
-        matchedTasks = getMatchingTasks_serverSide(searchTerm);
-        console.log(matchedTasks);
-        displayExistingTasks(section, matchedTasks);
+        var matchedTasks = getMatchingTasks_serverSide(searchTerm);
+        THESITENAME.MATCHING_TASKS.set(matchedTasks);
+        THESITENAME.MATCHING_TASKS.display();
     }
     else {
         //the search term IS an empty string
+        var displayedTaskButtons = THESITENAME.DISPLAYED_TASKS.get();
+
         if(displayedTaskButtons.length > 0) {
-            for (var i=0; i<displayedTaskButtons.length; i++) {
-                removeTaskButton(displayedTaskButtons[i]);
-            }
+            $(displayedTaskButtons).remove();
         }
         else {
-            removeNoMatchingTasksText(section);
+            THESITENAME.NO_MATCHING_TASKS.remove();
         }
     }
 }
 
-function displayNoMatchingTasksText(section) {
-    section.find(".content_wrapper").append($('<p>')
-        .text("no results were found")
-        .addClass("noMatchingTasks"));
-
-    THESITENAME.set_isDisplayedNoMatchingTasksText(true);
-}
-
-function removeNoMatchingTasksText(section) {
-    section.find(".noMatchingTasks").remove();
-    THESITENAME.set_isDisplayedNoMatchingTasksText(false);
-}
-
-
-function displayExistingTasks(section, matchedTasks) {
-
-    checkForUnnecessaryTaskButtons(section, matchedTasks);
-
-    if( (!matchedTasks || matchedTasks.length === 0)  && !THESITENAME.isDisplayedNoMatchingTasksText() ) {
-        // there are no matched tasks
-        displayNoMatchingTasksText(section);
-    }
-    else {
-        //there are matched tasks
-        for (var i=0; i<matchedTasks.length; i++) {
-            if(! isTaskButtonDisplayed(section, matchedTasks[i]) ) {
-                addTaskButton(section, matchedTasks[i]);
-            }
-        }
-    }
-}
-
-function checkForUnnecessaryTaskButtons(section, matchedTasks) {
-    var displayedTaskButtons = section.find(".taskButton");
-
-    if( matchedTasks.length === 0 ) {
-        //there are no matched tasks, so remove the ones remained from the previous search term
-        displayedTaskButtons.remove();
-    }
-    else {
-
-        if( THESITENAME.isDisplayedNoMatchingTasksText() ) {
-            // this text is present from the previous search but now there are matching tasks
-            // and the text has to be removed
-            removeNoMatchingTasksText(section);
-        }
-
-        if ( displayedTaskButtons.length > matchedTasks.length ) {
-            console.log("there are unnecessary task buttons");
-            // remove those displayed buttons that are unnecessary
-            for (var i=0; i<displayedTaskButtons.length; i++) {
-                var unnecessaryTask = false;
-
-                for (var j=0; j<matchedTasks.length; j++) {
-                    console.log("in the second for");
-
-                    if( $(displayedTaskButtons[i]).val() === matchedTasks[j] ) {
-                        console.log("matching found: " + $(displayedTaskButtons[i]).val() );
-                        unnecessaryTask = false;
-                        break;
-                    }
-                    else {
-                        unnecessaryTask = true;
-                    }
-                }
-                if (unnecessaryTask) {
-                    console.log("unnecessary task found!");
-                    removeTaskButton($(displayedTaskButtons[i]));
-                }
-            }
-        }
-    }
-}
-
-function isTaskButtonDisplayed(section, task) {
-    var displayedTaskButtons = section.find(".taskButton");
-    for (var i=0; i<displayedTaskButtons.length; i++) {
-        if( $(displayedTaskButtons[i]).val() === task ) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function addTaskButton(section, task) {
-    section.find(".content_wrapper").append($('<input>')
-        .prop('type', 'button')
-        .val("" + task)
-        .addClass("taskButton")
-    );
-}
-
-function removeTaskButton(task) {
-    console.log("in remove task: " + $(task).val());
-    //section.find(".taskButton[value='spalat WC']").remove();
-    task.remove();
-}
+//
+//THE NEXT FUNCTIONS ARE TO BE IMPLEMENTED ON THE SERVER SIDE
+//
 
 function getMatchingTasks_serverSide(searchTerm) {
     var allTasks = getAllTasks_serverSide();
@@ -134,6 +39,9 @@ function getMatchingTasks_serverSide(searchTerm) {
 }
 
 function getAllTasks_serverSide() {
-    var allTasks = ["spalat WC", "spalat masina", "sapun", "deparazitare caine"];
-    return allTasks;
+    return ["spalat WC", "spalat masina", "sapun", "deparazitare caine"];
 }
+
+//
+//END OF FUNCTIONS TO BE IMPLEMENTED ON THE SERVER SIDE
+//
